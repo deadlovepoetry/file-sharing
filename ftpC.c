b700d0b9f4fb711eca4527c078ac11df3312a7b7
Anamitra <anamitrahazra88915@gmail.com>
2024-11-08 16:57:04 +0530

    user auth

--- ftpC.c
+++ ftpC.c
@@ -1,338 +1,253 @@
-// Name : Anamitra Hazra
-// Roll: 22CS8028
-
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/sendfile.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <pthread.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/types.h>
 
-#include <dirent.h>
-#include <stdio.h>
-#define CLIENT_DATA_PORT 5500
-#define SERVER_CONTROL_PORT 5000
+#define SERVER_CONTROL_PORT 4000
+#define CLIENT_DATA_PORT 5100
 #define MAX 81
 
+void displayMenu();
+void signup(int clientC_FD);
+void login(int clientC_FD);
+void handleClientConnection(int clientC_FD);
+void checkStatusCode(char *response, int clientC_FD);
+void getFile(int clientC_FD, const char *filename);
+void sendFile(int clientC_FD, const char *filename);
+void parseFileName(const char *command, char *filename, int offset);
 
+void signup(int clientC_FD) {
+    char buffer[MAX];
+    char username[25], password[25];
 
-void parseFileName(char *buffer, char *filename, int i)
-{
-    strcpy(filename,buffer+i);
+    printf("Sign Up\n");
+    printf("Enter username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = 0;
+
+    printf("Enter password: ");
+    fgets(password, sizeof(password), stdin);
+    password[strcspn(password, "\n")] = 0;
+
+    snprintf(buffer, MAX, "signup %s %s", username, password);
+    send(clientC_FD, buffer, strlen(buffer), 0);
+    recv(clientC_FD, buffer, MAX, 0);
+    printf("%s\n", buffer);
 }
 
-int getClientDataSocket()
-{
-    int socketFD;
+void login(int clientC_FD) {
+    char buffer[MAX];
+    char username[50], password[50];
+
+    printf("Login\n");
+    printf("Enter username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = 0;
+
+    printf("Enter password: ");
+    fgets(password, sizeof(password), stdin);
+    password[strcspn(password, "\n")] = 0;
+
+    // Limit the length to avoid buffer overflow
+    snprintf(buffer, MAX, "login %.30s %.30s", username, password);
+
+    printf("Sending login data: %s\n", buffer);  // Debugging statement
+    if (send(clientC_FD, buffer, strlen(buffer), 0) == -1) {
+        perror("Error sending login data");
+        return;
+    }
+
+    // Clear buffer and receive response
+    memset(buffer, 0, sizeof(buffer));
+    int bytesReceived = recv(clientC_FD, buffer, MAX, 0);
+
+    if (bytesReceived < 0) {
+        perror("Error receiving server response");
+        return;
+    } else if (bytesReceived == 0) {
+        printf("Server disconnected\n");
+        return;
+    }
+
+    printf("Received response: %s\n", buffer);  // Debugging statement
+
+    // Check response for successful login
+    if (strcmp(buffer, "200 OK") == 0) {
+        printf("Login successful!\n");
+        handleClientConnection(clientC_FD);
+    } else {
+        printf("Login failed: %s\n", buffer);
+    }
+}
+
+void displayMenu() {
+    printf("1. Sign Up\n");
+    printf("2. Login\n");
+    printf("3. Quit\n");
+}
+
+int main() {
+    int clientC_FD;
     struct sockaddr_in server_addr;
 
-    // socket creation
-    socketFD = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketFD < 0)
-    {
-        printf("socket creation failed..\n");
-        exit(0);
+    clientC_FD = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientC_FD < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // allowing reuse of address and port
-    if (setsockopt(socketFD, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
-        perror("setsockopt(SO_REUSEADDR) failed");
-
-    bzero(&server_addr, sizeof(server_addr));
-
-    // assigning ip and port
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(CLIENT_DATA_PORT);
+    server_addr.sin_port = htons(SERVER_CONTROL_PORT);
 
-    // binding socket to ip
-    if ((bind(socketFD, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0))
-    {
-        printf("Bind failed");
-        exit(0);
-    }
-
-    return socketFD;
-}
-
-void checkStatusCode(char *response, int clientC_FD)
-{
-    int code = atoi(response);
-
-    switch (code)
-    {
-
-    case 200:
-    {
-        printf("\nsuccessful\n\n");
-        break;
-    }
-    case 503:
-    {
-        printf("\ninvalid server request\n\n");
+    if (connect(clientC_FD, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error connecting to server");
         close(clientC_FD);
-        exit(0);
-    }
-    case 550:
-    {
-        printf("\ninvalid request\n\n");
-        close(clientC_FD);
-        exit(0);
-    }
-    case 201:
-    {
-        printf("\n file exists and ready for transfer\n\n");
-        break;
-    }
-    case 501:
-    {
-        printf("\nerror changing directory\n");
-        break;
-    }
-    }
-    return;
-}
-
-void getFile(int clientC_FD,char *buffer)
-{
-    
-    char response[MAX];
-    bzero(response, MAX);
-
-    struct sockaddr_in server_data_addr;
-    int serverD_FD, clientD_FD = getClientDataSocket();
-
-    if (listen(clientD_FD, 10) < 0)
-    {
-        printf("Data port listening error\n");
-        return;
-    }
-    else
-    {
-        printf("Data port started listening\n");
+        exit(EXIT_FAILURE);
     }
 
-    int len = sizeof(server_data_addr);
-    serverD_FD = accept(clientD_FD, (struct sockaddr *)&server_data_addr, &len);
-    if (serverD_FD < 0)
-    {
-        printf("error accepting server data port\n");
-        close(clientD_FD);
-        return;
-    }
+    printf("Connected to server\n");
 
-    char filename[100];
-
-    //parseFileName(buffer,filename,4);
-    FILE *fp = fopen("received.txt", "wb");  // Open file for writing in binary mode
+    int choice;
     while (1) {
-        int bytes_recv = recv(serverD_FD, buffer, MAX, 0);
-        //printf("%d\n",bytes_recv);
-        if (bytes_recv <= 0) {
-            break;
-        }
+        displayMenu();
+        printf("Select option: ");
+        scanf("%d", &choice);
+        getchar();
 
-        char flag = buffer[0];
-        short data_length = ntohs(*(short*)(buffer + 1));
-
-        // write data into file
-        fwrite(buffer + 3, sizeof(char), data_length, fp);  // Write received data to file
-        if (flag == 'L') {
-            // Last block received, break out of loop
+        if (choice == 1) {
+            signup(clientC_FD);
+        } else if (choice == 2) {
+            login(clientC_FD);
+        } else if (choice == 3) {
+            printf("Quitting...\n");
             break;
+        } else {
+            printf("Invalid option\n");
         }
     }
 
-    fclose(fp);
-    close(clientD_FD);
+    close(clientC_FD);
+    return 0;
 }
 
-void sendFile(char * filename)
-{
-    char buffer[MAX];
-    bzero(buffer, MAX);
-
-    struct sockaddr_in server_data_addr;
-    int serverD_FD, clientD_FD = getClientDataSocket();
-
-    if (listen(clientD_FD, 10) < 0)
-    {
-        printf("Data port listening error\n");
-        return;
-    }
-    else
-    {
-        printf("Data port started listening\n");
-    }
-
-    int len = sizeof(server_data_addr);
-    serverD_FD = accept(clientD_FD, (struct sockaddr *)&server_data_addr, &len);
-    if (serverD_FD < 0)
-    {
-        printf("error accepting server data port\n");
-        close(clientD_FD);
-        return;
-    }
-
-    // send blocks of data 
-    FILE *fp = fopen(filename,"rb");
-    int bytes_read;
-    while ((bytes_read = fread(buffer + 3, sizeof(char), MAX, fp)) > 0) {
-        // Set flag character based on remaining data
-        buffer[0] = (feof(fp)) ? 'L' : '*';  // 'L' for last block, '*' for others
-
-        // Convert data length (excluding header) to network byte order
-        // | L/* | length | length | data | data...
-        short data_length = htons(bytes_read);
-        memcpy(buffer + 1, &data_length, sizeof(short));
-
-        // Send the entire block (header + data)
-        int bytes_sent = write(serverD_FD, buffer, MAX);
-        if (bytes_sent < 0) {
-            perror("send failed");
-            break;
-        }
-    }
-
-
-}
-
-void handleClientConnection(int clientC_FD)
-{
+void handleClientConnection(int clientC_FD) {
     char buffer[MAX];
     char response[MAX];
 
     bzero(buffer, MAX);
-    sprintf(buffer, "%d", CLIENT_DATA_PORT);
-
-    // send port Y to server
+    snprintf(buffer, sizeof(buffer), "%d", CLIENT_DATA_PORT);
     write(clientC_FD, buffer, sizeof(buffer));
     bzero(response, MAX);
 
     recv(clientC_FD, response, sizeof(response), 0);
     checkStatusCode(response, clientC_FD);
 
-    // options for the client
-    while (1)
-    {
+    while (1) {
         bool flag = false;
         int ch;
-        printf(" 1. To change server directory\n 2. get file from server \n 3. send file to server\n 4. quit\n");
+        printf(" 1. To change server directory\n 2. Get file from server\n 3. Send file to server\n 4. Quit\n");
+        printf("Enter your choice: ");
         scanf("%d", &ch);
         getchar();
         bzero(buffer, MAX);
-        switch (ch)
-        {
-        case 1:
-        {
-            printf("enter the command to change directory (like > cd path)\n");
-            fflush(stdin);
-            fgets(buffer, sizeof(buffer), stdin);  
-            
-            // Remove trailing newline (optional)
-            buffer[strcspn(buffer, "\n")] = '\0'; 
+        
+        switch (ch) {
+            case 1:
+                printf("Enter the command to change directory (like > cd path):\n");
+                fgets(buffer, sizeof(buffer), stdin);
+                buffer[strcspn(buffer, "\n")] = 0;
+                write(clientC_FD, buffer, sizeof(buffer));
+                recv(clientC_FD, response, sizeof(response), 0);
+                checkStatusCode(response, clientC_FD);
+                break;
 
-            // send the command to the server
-            write(clientC_FD, buffer, sizeof(buffer));
+            case 2:
+                printf("Enter the command to get file (like > get filename):\n");
+                fgets(buffer, sizeof(buffer), stdin);
+                buffer[strcspn(buffer, "\n")] = 0;
+                write(clientC_FD, buffer, sizeof(buffer));
+                recv(clientC_FD, response, sizeof(response), 0);
+                checkStatusCode(response, clientC_FD);
+                getFile(clientC_FD, buffer);
+                break;
 
-            // receive status code
-            recv(clientC_FD, response, sizeof(response), 0);
-            checkStatusCode(response, clientC_FD);
+            case 3:
+                printf("Enter the command to send file (like > put filename):\n");
+                fgets(buffer, sizeof(buffer), stdin);
+                buffer[strcspn(buffer, "\n")] = 0;
+                write(clientC_FD, buffer, sizeof(buffer));
+                char filename[20];
+                parseFileName(buffer, filename, 4);
+                sendFile(clientC_FD, filename);
+                break;
 
-            break;
+            case 4:
+                flag = true;
+                printf("Disconnecting...\n");
+                break;
+
+            default:
+                printf("Invalid option\n");
         }
-        case 2:
-        {
-            // ask user for file name
-            printf("enter the command (like > get filename)\n");
-            fflush(stdin);
-            //scanf("%[^\n]s",buffer);
-            //read(stdin,buffer,sizeof(buffer));
-            fgets(buffer, sizeof(buffer), stdin);  
-            
-            // Remove trailing newline (optional)
-            buffer[strcspn(buffer, "\n")] = '\0'; 
 
-            // send the command to the server
-            write(clientC_FD, buffer, sizeof(buffer));
-
-            // receive status code
-            recv(clientC_FD, response, sizeof(response), 0);
-            checkStatusCode(response, clientC_FD);
-
-            //receive the file from the server
-            getFile(clientC_FD,buffer);
-            break;
-        }
-        case 3:
-        {
-            // ask user for file name
-            printf("enter the command (like > put filename)\n");
-            fflush(stdin);
-            //scanf("%[^\n]s",buffer);
-            //read(stdin,buffer,sizeof(buffer));
-            fgets(buffer, sizeof(buffer), stdin);  
-            
-            // Remove trailing newline (optional)
-            buffer[strcspn(buffer, "\n")] = '\0'; 
-
-            // send the command to the server
-            write(clientC_FD, buffer, sizeof(buffer));
-
-            // parse filename
-            char filename[20];
-            parseFileName(buffer,filename,4);
-
-            // send file function
-            sendFile(filename);
-            break;
-        }
-        case 4:
-        {
-            flag = true;
-            break;
-        }
-        }
-        if (flag)
-            break;
+        if (flag) break;
     }
+
     close(clientC_FD);
 }
 
-int main()
-{
-    int clientC_FD;
-    struct sockaddr_in server_addr;
-
-    clientC_FD = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (clientC_FD < 0)
-    {
-        printf("failed to create client socket\n");
-        exit(0);
+void checkStatusCode(char *response, int clientC_FD) {
+    if (strcmp(response, "200 OK") == 0) {
+        printf("Operation successful\n");
+    } else {
+        printf("Error: %s\n", response);
     }
-
-    bzero(&server_addr, sizeof(server_addr));
-
-    // assign server ip and port
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(SERVER_CONTROL_PORT);
-
-    // connect client with server
-    if (connect(clientC_FD, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        printf("error connecting with server...\n");
-        exit(0);
-    }
-    else
-        printf("connected with server..\n");
-
-    handleClientConnection(clientC_FD);
 }
+
+void getFile(int clientC_FD, const char *filename) {
+    FILE *file = fopen(filename, "wb");
+    if (file == NULL) {
+        perror("File open error");
+        return;
+    }
+    
+    char buffer[MAX];
+    int bytes;
+    while ((bytes = recv(clientC_FD, buffer, MAX, 0)) > 0) {
+        fwrite(buffer, 1, bytes, file);
+        if (bytes < MAX) break;
+    }
+    
+    fclose(file);
+    printf("File received: %s\n", filename);
+}
+
+void sendFile(int clientC_FD, const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("File open error");
+        return;
+    }
+    
+    char buffer[MAX];
+    int bytes;
+    while ((bytes = fread(buffer, 1, MAX, file)) > 0) {
+        send(clientC_FD, buffer, bytes, 0);
+    }
+    
+    fclose(file);
+    printf("File sent: %s\n", filename);
+}
+
+void parseFileName(const char *command, char *filename, int offset) {
+    strncpy(filename, command + offset, strlen(command) - offset);
+    filename[strlen(command) - offset] = '\0';
+}
+
--- ftpS.c
+++ ftpS.c
@@ -1,309 +1,139 @@
-// Name: Anamitra Hazra
-// Roll: 22CS8028
-
+// Updated server code with login and signup features
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/sendfile.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <pthread.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/types.h>
 
-#include <dirent.h>
-#include <stdio.h>
-#define SERVER_CONTROL_PORT 5000
-#define CLIENT_DATA_PORT 5500
+#define SERVER_CONTROL_PORT 4000
+#define CLIENT_DATA_PORT 5100
 #define MAX 81
+#define USER_DB "user_db.txt"
 
+bool authenticateUser(char *username, char *password);
+bool registerUser(char *username, char *password);
+void handleClientRequest(int clientC_FD);
+void handleUserCommands(int clientC_FD);
 
-
-bool isDigit(char num)
-{
-    return (num >= '0' && num <= '9');
-}
-
-void parseFileName(char *buffer, char *filename, int i)
-{
-    strcpy(filename,buffer+i); // first i blocks contain command and space
-}
-
-// function to verify port of the client
-int verifyPort(char *buffer)
-{
-    int len = sizeof(buffer);
-    int port = atoi(buffer);
-    /*for (int i = 0; i < len && buffer[i] != '\0'; i++)
-    {
-        if (!isDigit(buffer[i]))
-        {
-            return 503; // invalid client response
-        }
-        port = port * 10 + (buffer[i] - '0');
-    }*/
-
-    if (port >= 1024 && port <= 65535)
-    {
-        printf("Port Y: %d\n",port);
-        return 200;
-    }
-    else
-        return 550; // port invalid
-}
-
-int getServerDataSocket()
-{
-    int sockFD;
-
-    sockFD = socket(AF_INET,SOCK_STREAM,0);
-    if(sockFD<0)
-    {
-        printf("failed to create server data socket\n");
-        exit(0);
+bool authenticateUser(char *username, char *password) {
+    FILE *fp = fopen(USER_DB, "r");
+    if (fp == NULL) {
+        perror("Error opening user database");
+        return false;
     }
 
-    return sockFD;
-}
-
-// send file to the client
-void sendFileFunc(FILE *fp)
-{
-    char buffer[MAX];
-    int bytes_read;
-    struct sockaddr_in client_addr;
-
-    // create server data socket
-    int serverD_FD = getServerDataSocket();
-    bzero(&client_addr,sizeof(client_addr));
-
-    // assign port and ip
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    client_addr.sin_port = htons(CLIENT_DATA_PORT);
-
-    //connect with the client data port
-    if(connect(serverD_FD,(struct sockaddr *)&client_addr,sizeof(client_addr))<0)
-    {
-        printf("error connecting with server...\n");
-        exit(0);
-    }
-
-    // send blocks of data 
-    while ((bytes_read = fread(buffer + 3, sizeof(char), MAX, fp)) > 0) {
-        // Set flag character based on remaining data
-        buffer[0] = (feof(fp)) ? 'L' : '*';  // 'L' for last block, '*' for others
-
-        // Convert data length (excluding header) to network byte order
-        // | L/* | length | length | data | data...
-        short data_length = htons(bytes_read);
-        memcpy(buffer + 1, &data_length, sizeof(short));
-
-        // Send the entire block (header + data)
-        int bytes_sent = write(serverD_FD, buffer, MAX);
-        if (bytes_sent < 0) {
-            perror("send failed");
-            break;
-        }
-    }
-
-}
-
-// receive file from the client
-void getFile(char *filename)
-{
-    char buffer[MAX];
-    int bytes_read;
-    struct sockaddr_in client_addr;
-
-    // create server data socket
-    int serverD_FD = getServerDataSocket();
-    bzero(&client_addr,sizeof(client_addr));
-
-    // assign port and ip
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    client_addr.sin_port = htons(CLIENT_DATA_PORT);
-
-    //connect with the client data port
-    if(connect(serverD_FD,(struct sockaddr *)&client_addr,sizeof(client_addr))<0)
-    {
-        printf("error connecting with server...\n");
-        exit(0);
-    }
-
-    FILE *fp = fopen("server.txt", "wb");  // Open file for writing in binary mode
-    while (1) {
-        int bytes_recv = recv(serverD_FD, buffer, MAX, 0);
-        //printf("%d\n",bytes_recv);
-        if (bytes_recv <= 0) {
-            break;
-        }
-
-        char flag = buffer[0];
-        short data_length = ntohs(*(short*)(buffer + 1));
-
-        // write data into file
-        fwrite(buffer + 3, sizeof(char), data_length, fp);  // Write received data to file
-        if (flag == 'L') {
-            // Last block received, break out of loop
-            break;
+    char dbUsername[50], dbPassword[50];
+    while (fscanf(fp, "%s %s", dbUsername, dbPassword) != EOF) {
+        if (strcmp(dbUsername, username) == 0 && strcmp(dbPassword, password) == 0) {
+            fclose(fp);
+            return true;
         }
     }
 
     fclose(fp);
-    close(serverD_FD);
+    return false;
 }
 
-// control socket functionality
-void handleConnection(int clientC_FD)
-{
-    char buffer[MAX], server_response[MAX];
+bool registerUser(char *username, char *password) {
+    FILE *fp = fopen(USER_DB, "a");
+    if (fp == NULL) {
+        perror("Error opening user database");
+        return false;
+    }
+
+    fprintf(fp, "%s %s\n", username, password);
+    fclose(fp);
+    return true;
+}
+
+void handleClientRequest(int clientC_FD) {
+    char buffer[MAX], response[MAX];
     bzero(buffer, MAX);
-    bzero(server_response, MAX);
-    // receive port Y from client
+
     recv(clientC_FD, buffer, sizeof(buffer), 0);
 
-    int code = verifyPort(buffer);
-    sprintf(server_response, "%d", code);
-
-    // send code
-    write(clientC_FD, server_response, sizeof(server_response));
-    //printf("Connected with client\n");
-
-    while (1)
-    {
-        bzero(buffer, MAX);
-        recv(clientC_FD, buffer, sizeof(buffer), 0);
-
-        // options on server
-        if (buffer[0] == 'c' && buffer[1] == 'd')
-        {
-            char cmd[10];
-            strcpy(cmd,buffer+3);
-            if(chdir(cmd)<0)
-            {
-                strcpy(server_response,"501");
-                write(clientC_FD,server_response,sizeof(server_response));
-                break;
-            }
-            else
-            {
-                char dir[100];
-                printf("%s\n",getcwd(dir,sizeof(dir)));
-                strcpy(server_response,"200");
-                write(clientC_FD,server_response,sizeof(server_response));
-            }
-            
+    if (strncmp(buffer, "signup", 6) == 0) {
+        char *username = strtok(buffer + 7, " ");
+        char *password = strtok(NULL, " ");
+        if (registerUser(username, password)) {
+            strcpy(response, "User registered successfully");
+        } else {
+            strcpy(response, "Failed to register user");
         }
-        else if (buffer[0] == 'g' && buffer[1] == 'e' && buffer[2] == 't')
-        {
-            char filename[20];
-            //parse filename
-            parseFileName(buffer,filename,4);
-            printf("%s\n",filename);
-            FILE* fp =fopen(filename,"rb");
-            if(fp == NULL)
-            {
-                strcpy(server_response,"550");
-                write(clientC_FD,server_response,sizeof(server_response));
-                break;
-            }
-            else
-            {
-                strcpy(server_response,"201");
-                write(clientC_FD,server_response,sizeof(server_response));
-            }
-            sleep(1); // wait for client to create data socket
-
-            sendFileFunc(fp);
-            fclose(fp);
-
+    } else if (strncmp(buffer, "login", 5) == 0) {
+        char *username = strtok(buffer + 6, " ");
+        char *password = strtok(NULL, " ");
+        if (authenticateUser(username, password)) {
+            strcpy(response, "200 OK");
+            send(clientC_FD, response, strlen(response), 0);
+            handleUserCommands(clientC_FD);
+            return;
+        } else {
+            strcpy(response, "401 Unauthorized");
         }
-        else if (buffer[0] == 'p' && buffer[1] == 'u' && buffer[2] == 't')
-        {
-
-            char filename[20];
-            //parse filename
-            parseFileName(buffer,filename,4);
-
-            sleep(1);
-
-            getFile(filename);
-
-        }
-        else if (buffer[0] == 'q' && buffer[1] == 'u' && buffer[2] == 'i' && buffer[3] == 't')
-        {
-            strcpy(server_response,"421");
-
-            write(clientC_FD, server_response, sizeof(server_response));
-            break;
-        }
-        else
-        {
-            bzero(server_response, MAX);
-            strcat(server_response, "502"); // invalid request
-
-            write(clientC_FD, server_response, sizeof(server_response));
-        }
+    } else {
+        strcpy(response, "400 Bad Request");
     }
-    return;
+
+    send(clientC_FD, response, strlen(response), 0);
 }
 
-int main()
-{
-    int sockC_FD, clientC_FD, len;
-    struct sockaddr_in server_addr, client_addr;
+void handleUserCommands(int clientC_FD) {
+    char buffer[MAX];
+    while (1) {
+        bzero(buffer, MAX);
+        recv(clientC_FD, buffer, sizeof(buffer), 0);
+        if (strcmp(buffer, "quit") == 0) {
+            break;
+        } else if (strncmp(buffer, "get", 3) == 0) {
+            // Handle file transfer
+        } else if (strncmp(buffer, "put", 3) == 0) {
+            // Handle file upload
+        } else {
+            send(clientC_FD, "502 Command not implemented", 27, 0);
+        }
+    }
+}
 
-    // socket creation
+int main() {
+    int sockC_FD, clientC_FD;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
     sockC_FD = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockC_FD < 0)
-    {
-        printf("socket creation failed..\n");
+    if (sockC_FD < 0) {
+        perror("Socket creation failed");
         exit(0);
     }
 
-    // allowing reuse of address and port
-    if (setsockopt(sockC_FD, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
-        perror("setsockopt(SO_REUSEADDR) failed");
-
-    bzero(&server_addr, sizeof(server_addr));
-
-    // assigning ip and port
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(SERVER_CONTROL_PORT);
 
-    // binding socket to ip
-    if ((bind(sockC_FD, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0))
-    {
-        printf("Bind failed");
+    if (bind(sockC_FD, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
         exit(0);
     }
 
-    // listen for connections
-    if (listen(sockC_FD, 10))
-    {
-        printf("Listening error..");
+    if (listen(sockC_FD, 10) < 0) {
+        perror("Listen failed");
         exit(0);
     }
-    printf("server listening... \n");
 
-    len = sizeof(client_addr);
-    while (clientC_FD = accept(sockC_FD, (struct sockaddr *)&client_addr, &len))
-    {
-        
-        handleConnection(clientC_FD);
-    }
+    printf("Server listening on port %d...\n", SERVER_CONTROL_PORT);
 
-    if (clientC_FD < 0)
-    {
-        printf("Error accepting client connection\n");
-        exit(0);
+    while ((clientC_FD = accept(sockC_FD, (struct sockaddr *)&client_addr, &client_len)) >= 0) {
+        printf("Client connected\n");
+        handleClientRequest(clientC_FD);
+        close(clientC_FD);
     }
 
     close(sockC_FD);
+    return 0;
 }
+
